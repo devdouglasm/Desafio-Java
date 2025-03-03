b05b9b804738a219b80b82e878673c04707b2a15
devdouglasm <devdouglasm@gmail.com>
2025-03-03 18:43:45 -0300

    initial commit

---
+++ src/test/java/com/dmdev/injecao_dependencia/InjecaoDependenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmdev.injecao_dependencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjecaoDependenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
