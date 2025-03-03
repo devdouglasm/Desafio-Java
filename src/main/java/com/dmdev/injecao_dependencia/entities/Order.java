076885208556c6a19e8247f2161200e798986120
devdouglasm <devdouglasm@gmail.com>
2025-03-03 19:14:50 -0300

    criacao dos services

--- src/main/java/com/dmdev/injecao_dependencia/entities/Order.java
+++ src/main/java/com/dmdev/injecao_dependencia/entities/Order.java
@@ -1,4 +1,38 @@
 package com.dmdev.injecao_dependencia.entities;
 
 public class Order {
+
+    private Integer code;
+    private Double basic;
+    private Double discount;
+
+    public Order(Integer code, Double basic, Double discount) {
+        this.code = code;
+        this.basic = basic;
+        this.discount = discount;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Double getBasic() {
+        return basic;
+    }
+
+    public void setBasic(Double basic) {
+        this.basic = basic;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
 }
---
+++ src/main/java/com/dmdev/injecao_dependencia/services/OrderService.java
@@ -0,0 +1,18 @@
+package com.dmdev.injecao_dependencia.services;
+
+import com.dmdev.injecao_dependencia.entities.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    ShippingService shippingService;
+
+    public double total(Order order) {
+        double totalDiscount = order.getBasic() * order.getDiscount() / 100;
+        return order.getBasic() - totalDiscount;
+    }
+
+}
